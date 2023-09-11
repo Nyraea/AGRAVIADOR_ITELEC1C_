70370dd1529fd07861ace53c0b19e9791d67eabd
johannes yees <124044259+meatbucket123@users.noreply.github.com>
2023-09-11 12:38:50 +0800

    Initial commit

---
+++ Agraviador_ITELEC1C/Controllers/InstructorController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Agraviador_ITELEC1C.Models;
+
+namespace Agraviador_ITELEC1C.Controllers
+{
+    public class InstructorController : Controller
+    {
+        List<Instructor> InstructorList = new List<Instructor>
+            {
+                new Instructor()
+                {
+                    Id= 1,FirstName = "Maria",LastName = "Edang", Rank = Rank.Professor, HiringDate = DateTime.Parse("2022-01-04"), IsTenured = true
+                },
+                new Instructor()
+                {
+                    Id= 2,FirstName = "Jobert",LastName = "Magtamo", Rank = Rank.AssistantProfessor, HiringDate = DateTime.Parse("2020-08-26"), IsTenured = false
+                },
+                new Instructor()
+                {
+                    Id= 3,FirstName = "Sinturon",LastName = "Dela Cruz", Rank = Rank.AssociateProfessor, HiringDate = DateTime.Parse("2019-04-16"), IsTenured = false
+                }
+            };
+        public IActionResult Index()
+        {
+
+            return View(InstructorList);
+        }
+
+        public IActionResult ShowDetail(int id)
+        {
+            //Search for the student whose id matches the given id
+            Instructor? instructor = InstructorList.FirstOrDefault(st => st.Id == id);
+
+            if (instructor != null)//was an student found?
+                return View(instructor);
+
+            return NotFound();
+        }
+
+    }
+}
+
